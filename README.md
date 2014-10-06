6b74f585e4e6edb40631843d75aa20a88d6c2e8b
gardenofwine <gardenofwine@gmail.com>
2014-10-06 23:58:43 +0300

    Integrate gulp, jade

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ heroku labs:enable websockets
 git push heroku master
 heroku open
 ```
+
+# TODO
+
+ 1. When server connects after the client, hook them together
+ 1. When client disconnects and another connects, hook server
+ 1. Support text areas (refactor UIView handling)
+ 1. read server from plist or some config file in hosting app
+ 1. only send to client modified labels
+ 1. refactor
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var jade = require('gulp-jade');
 
-gulp.task('default', function() {
+gulp.task('templates', function() {
+    var YOUR_LOCALS = {};
+    gulp.src('./source/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS,
+            pretty: true
+        }))
+        .pipe(gulp.dest('./wwwroot/'))
+});
+
+gulp.task('stylesheets', function() {
+    gulp.src('./source/stylesheets/*.css')
+        .pipe(gulp.dest('./wwwroot/stylesheets/'));
+});
+
+gulp.task('default', ['templates', 'stylesheets'], function() {
     // place code for your default task here
 });
