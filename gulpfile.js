15c595d7648883522c3a85b0927a02b102ab968d
gardenofwine <gardenofwine@gmail.com>
2014-10-29 18:10:35 +0200

    gulp - set build script with update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('templates', function() {
     var YOUR_LOCALS = {};
@@ -9,6 +10,13 @@ gulp.task('templates', function() {
             pretty: true
         }))
         .pipe(gulp.dest('./wwwroot/'))
+});
+
+gulp.task('develop',['stylesheets', 'scripts', 'templates'], function () {
+    nodemon({ script: 'app/server.js', watch: ['source/**/*.*']})
+        .on('restart', function () {
+            console.log('restarted!')
+        })
 });
 
 gulp.task('stylesheets', function() {
@@ -21,6 +29,11 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('./wwwroot/javascript/'));
 });
 
-gulp.task('default', ['templates', 'scripts', 'stylesheets'], function() {
+gulp.task('watch', function(){
+    gulp.watch('./source/javascript/*.js', ['scripts']);
+    gulp.watch('./source/stylesheets/*.css', ['stylesheets']);
+});
+
+gulp.task('default', ['templates', 'scripts', 'stylesheets', 'watch', 'develop'], function() {
     // place code for your default task here
 });
