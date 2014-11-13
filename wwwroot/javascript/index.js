4878a66714dd9c4cdcd3d8aa8e7a16952ed94193
gardenofwine <gardenofwine@gmail.com>
2014-11-14 01:59:43 +0200

    server refactor; create component handlers

--- wwwroot/javascript/index.js
+++ wwwroot/javascript/index.js
@@ -3,48 +3,22 @@ var BetterContent = {};
 BetterContent.config = {
     host: location.origin.replace(/^http/, 'ws')
 }
-BetterContent.App = function(){
-}
-BetterContent.App.prototype.ws =  null;
+BetterContent.App = function(){};
 
 BetterContent.App.prototype.onMessage = function(event){
     console.log("Received Message");
     console.log(event);
 
     var nativeContent = JSON.parse(event.data);
-    var contentClass = nativeContent.class;
-
     var elementsDiv = document.querySelector('#nativeContent')
     elementsDiv.innerHTML = '';
     for (var i = 0; i < nativeContent.length; i++) {
-        var element = document.createElement('div');
-        var frame = nativeContent[i].attributes.frame;
-        var font = nativeContent[i].attributes.font;
-
-        element.innerHTML = nativeContent[i].attributes.text;
-        element.setAttribute('class', 'element');
-        element.setAttribute('contenteditable', true);
-        element.setAttribute("key", nativeContent[i].key);
-
-        var positionStyle =
-            "top:" + frame.Y + "px; left:" + frame.X + "px; width:" + frame.Width + "px; height:" + frame.Height + "px;" +
-            "font-size:" + font.pointSize + "px;";
-        element.setAttribute('style', positionStyle);
-        elementsDiv.appendChild(element);
-
-        element.addEventListener("input", function (event) {
-            console.log(event.srcElement);
-            sendLabel(event.srcElement);
-        }, false);
+        var componentClass = nativeContent[i].attributes.class;
+        var componentHandler = BetterContent.Components[componentClass];
+        if (typeof componentHandler !== "undefined"){
+            componentHandler.draw(nativeContent[i], elementsDiv);
+        }
     }
-
-    var sendLabel = function (element) {
-        var newLabelMap = [];
-        var value = element.innerText;
-        newLabelMap.push({'key': element.getAttribute('key'), 'attributes': {'text': value}});
-        BetterContent.ws.send(JSON.stringify({'type': 'labelMap', 'data': newLabelMap}));
-    }
-
 }
 
 BetterContent.App.prototype.onOpen = function() {
@@ -76,6 +50,40 @@ BetterContent.App.prototype.setupWebSocket = function(){
     this.ws.onclose = this.onClose;
     BetterContent.ws = this.ws;
 }
+
+BetterContent.Components = {}
+
+BetterContent.Components.label = {
+    draw: function(component, elementsDiv){
+        var element = document.createElement('div');
+        var frame = component.attributes.frame;
+        var font = component.attributes.font;
+
+        element.innerHTML = component.attributes.text;
+        element.setAttribute('class', 'element');
+        element.setAttribute('contenteditable', true);
+        element.setAttribute("key", component.key);
+
+        var positionStyle =
+            "top:" + frame.Y + "px; left:" + frame.X + "px; width:" + frame.Width + "px; height:" + frame.Height + "px;" +
+            "font-size:" + font.pointSize + "px;";
+        element.setAttribute('style', positionStyle);
+        elementsDiv.appendChild(element);
+
+        element.addEventListener("input", function (event) {
+            console.log(event.srcElement);
+            sendLabel(event.srcElement);
+        }, false);
+
+        var sendLabel = function (element) {
+            var newLabelMap = [];
+            var value = element.innerText;
+            newLabelMap.push({'key': element.getAttribute('key'), 'attributes': {'text': value}});
+            BetterContent.ws.send(JSON.stringify({'type': 'labelMap', 'data': newLabelMap}));
+        }
+    }
+}
+
 
 var app = new BetterContent.App();
 app.setupWebSocket();
